c76eb33432c360aa30a235a6dd38e1ee12b73994
defstiller <81063003+defstiller@users.noreply.github.com>
2022-08-01 05:28:03 -0500

    added eslint extended by google, fixed eslint errors

--- index.js
+++ index.js
@@ -1,60 +1,45 @@
-import express from "express";
-import mongoose from "mongoose";
-import {PORT, MONGODB_URI} from "./config.js";
-import user from "./models/User.js";
-mongoose.connect(MONGODB_URI)
-.then(() => {
-	console.log("Connected to MongoDB");
-})
-.catch((err) => {
-	console.log("Error connecting to MongoDB: ", err.message);
+import express from 'express';
+import mongoose from 'mongoose';
+import {PORT, MONGODB_URI} from './config.js';
+import User from './models/User.js';
+mongoose
+    .connect(MONGODB_URI)
+    .then(() => {
+      console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+      console.log('Error connecting to MongoDB: ', err.message);
+    });
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  console.log(req.body.email);
+  res.send('Hello there!');
 });
-const app=express();
-app.use(express.json())
-// const userModel = mongoose.model("users", userSchema);
-
-app.get("/", (req, res) => {
-	console.log(req.body.email)
-	res.send("Hello there!") 
-})
-app.post("/auth/register", (req, res) => {
-	const userInstance = new user({
-		name: req.body.name,
-		email: req.body.email,
-		password: req.body.password
-	});
-	userInstance.save( (err, user) => {
-		if (err) {
-			res.status(500).send({
-				message: "Error registering user " + err.message
-			});
-		} else {
-			res.status(200).send({
-				message: "User created successfully"
-			});
-		}
-	}
-	)
-	 
-
-
-	// user.create({
-	// 	name: req.body.name,
-	// 	email: req.body.email,
-	// 	password: req.body.password
-	// })
-	// .then(() => {
-	// 	res.send("User created");
-	// })
-	// .catch((err) => {
-	// 	res.send(err.message);
-	// });
-})
+app.post('/auth/register', (req, res) => {
+  const userInstance = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  userInstance.save((err, user) => {
+    if (err) {
+      res.status(500).send({
+        message: 'Error registering user ' + err.message,
+      });
+    } else {
+      res.status(200).send({
+        message: 'User created successfully',
+      });
+    }
+  });
+});
 app.listen(PORT, (error) => {
-	if (error) {
-		console.log("Error: ", error);
-	} else {
-		console.log(`Server is listening on port ${PORT}`);
-	}
-}
-)
+  if (error) {
+    console.log('Error: ', error);
+  } else {
+    console.log(`Server is listening on port ${PORT}`);
+  }
+});
