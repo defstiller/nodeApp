d78d2d377009b3960fa36fefdf964e432a6b28be
defstiller <81063003+defstiller@users.noreply.github.com>
2022-08-02 00:19:31 -0500

    changed single quotes to double

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-const PORT = '5700';
-const MONGODB_URI = 'mongodb+srv://defstiller:Lionartem289002@cluster0.xrfil.mongodb.net/blog?retryWrites=true&w=majority';
+const PORT = "5700";
+const MONGODB_URI = "mongodb+srv://defstiller:Lionartem289002@cluster0.xrfil.mongodb.net/blog?retryWrites=true&w=majority";
 export {PORT, MONGODB_URI};
