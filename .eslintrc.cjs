a17d0760f8c2ef7f332ab402c84b2d4f8aa0c17b
defstiller <81063003+defstiller@users.noreply.github.com>
2022-08-02 00:17:26 -0500

    did not like google formating, used mine from previos project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,23 +1,23 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
-    'node': true,
-  },
-  'extends': [
-    'plugin:react/recommended',
-    'google',
-  ],
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
-    },
-    'ecmaVersion': 'latest',
-    'sourceType': 'module',
-  },
-  'plugins': [
-    'react',
-  ],
-  'rules': {
-  },
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: ["eslint:recommended", "plugin:react/recommended"],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	plugins: ["react"],
+	rules: {
+		indent: ["warn", "tab"],
+		quotes: ["warn", "double"],
+		semi: ["warn", "always"],
+		"no-unused-vars": "warn",
+		"react/forbid-prop-types": 0,
+	},
 };
