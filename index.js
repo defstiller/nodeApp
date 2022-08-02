728d69a68b45b72786ff594e4a6bd6770d5a30f0
defstiller <81063003+defstiller@users.noreply.github.com>
2022-08-02 00:21:01 -0500

    moved logic to authHelpers

--- index.js
+++ index.js
@@ -1,45 +1,37 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import {PORT, MONGODB_URI} from './config.js';
-import User from './models/User.js';
+import express from "express";
+import mongoose from "mongoose";
+import {PORT, MONGODB_URI} from "./config.js";
+import UserModel from "./models/User.js";
+import {loginValidation} from "./validations.js";
+import bcrypt from "bcrypt";
+import {loginHelper, signUpHelper} from "./helpers/authHelpers.js";
 mongoose
-    .connect(MONGODB_URI)
-    .then(() => {
-      console.log('Connected to MongoDB');
-    })
-    .catch((err) => {
-      console.log('Error connecting to MongoDB: ', err.message);
-    });
+	.connect(MONGODB_URI)
+	.then(() => {
+		console.log("Connected to MongoDB");
+	})
+	.catch((err) => {
+		console.log("Error connecting to MongoDB: ", err.message);
+	});
 
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  console.log(req.body.email);
-  res.send('Hello there!');
+app.get("/", (req, res) => {
+	console.log(req.body.email);
+	res.send("Hello there!");
 });
-app.post('/auth/register', (req, res) => {
-  const userInstance = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  });
-  userInstance.save((err, user) => {
-    if (err) {
-      res.status(500).send({
-        message: 'Error registering user ' + err.message,
-      });
-    } else {
-      res.status(200).send({
-        message: 'User created successfully',
-      });
-    }
-  });
+
+app.post("/auth/register", (req, res) => {
+	signUpHelper(UserModel, res, req);
+});
+app.post("/auth/login", loginValidation, (req, res) => {
+	loginHelper(UserModel, "Invalid email or password", "User logged in successfully", res, req);
 });
 app.listen(PORT, (error) => {
-  if (error) {
-    console.log('Error: ', error);
-  } else {
-    console.log(`Server is listening on port ${PORT}`);
-  }
+	if (error) {
+		console.log("Error: ", error);
+	} else {
+		console.log(`Server is listening on port ${PORT}`);
+	}
 });
