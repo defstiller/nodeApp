afdf892c369627b82f49c26a08ace6370f618ec0
defstiller <81063003+defstiller@users.noreply.github.com>
2022-08-02 00:21:15 -0500

    added login validation with express-validator

---
+++ validations.js
@@ -0,0 +1,13 @@
+import {body} from "express-validator";
+
+const loginValidation = [
+	body("email")
+		.isEmail()
+		.withMessage("Email must be valid"),
+	body("password")
+		.isLength({min: 6})
+		.withMessage("Password must be at least 6 characters"),
+];
+
+
+export {loginValidation};
